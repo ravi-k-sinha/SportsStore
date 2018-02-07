933d66e373a4283cadc0b00b5f9c45583dfa76f2
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-07 12:56:21 +0530

    Defined routes based on categories; removed earlier route; updated tag helper to use injected properties with a prefix; updated view to use prefixed property for category

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using SportsStore.Models.ViewModels;
+using System.Collections.Generic;
 
 
 namespace SportsStore.Infrastructure
@@ -26,6 +27,9 @@ namespace SportsStore.Infrastructure
 
         public string PageAction { get; set; }
 
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        private Dictionary<string, object> PageUrlValues { get; set; } = new Dictionary<string, object>();
+
         public bool PageClassesEnabled { get; set; } = false;
         public string PageClass { get; set; }
         public string PageClassNormal { get; set; }
@@ -33,13 +37,14 @@ namespace SportsStore.Infrastructure
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
-            TagBuilder result = new TagBuilder("div");
+            var urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            var result = new TagBuilder("div");
 
             for (int i = 1; i <= PageModel.TotalPages; i++)
             {
-                TagBuilder tag = new TagBuilder("a");
-                tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
+                var tag = new TagBuilder("a");
+                PageUrlValues["productPage"] = i;
+                tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
                 if (PageClassesEnabled)
                 {
                     tag.AddCssClass(PageClass);
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
 using Microsoft.Extensions.Configuration;
@@ -41,13 +42,33 @@ namespace SportsStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "pagination",
-                    template: "Products/Page{productPage}",
-                    defaults: new { Controller = "Product", action="List"});
+                    name: null,
+                    template: "{category}/Page{productPage:int}",
+                    defaults: new {controller = "Product", action = "List"}
+                );
+                
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Product}/{action=List}/{id?}"
-                    );
+                    name: null,
+                    template: "Page{productPage:int}",
+                    defaults: new {controller = "Product", action = "List", productPage = 1}
+                );
+                
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}",
+                    defaults: new {controller = "Product", action = "List", productPage = 1}
+                );
+                
+                routes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new {controller = "Product", action = "List", productPage = 1}
+                );
+                
+                routes.MapRoute(
+                    name: null,
+                    template: "{controller}/{action}/{id?}"
+                );
             });
             SeedData.EnsurePopulated(app);
         }

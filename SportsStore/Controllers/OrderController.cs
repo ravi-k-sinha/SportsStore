10eb95db2891aa6d0b4a6b1fe32c32c66dea492b
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-12 19:15:27 +0530

    Implented simple password based authentication for admin

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 
@@ -16,9 +17,12 @@ namespace SportsStore.Controllers
             _cart = cart;
         }
 
+        [Authorize]
         public ViewResult List() =>
             View(_repository.Orders.Where(o => !o.Shipped));
 
+        [HttpPost]
+        [Authorize]
         public IActionResult MarkShipped(int orderID)
         {
             var order = _repository.Orders
---
+++ SportsStore/Models/IdentitySeedData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    public class IdentitySeedData
+    {
+        private const string AdminUser = "Admin";
+        private const string AdminPassword = "Secret123$";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            var userManager = app.ApplicationServices.GetRequiredService<UserManager<IdentityUser>>();
+
+            var user = await userManager.FindByIdAsync(AdminUser);
+            if (user == null)
+            {
+                user = new IdentityUser("Admin");
+                await userManager.CreateAsync(user, AdminPassword);
+            }
+        }
+    }
+}

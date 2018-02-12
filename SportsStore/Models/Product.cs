9d5e4ac513b16cad383fb45b9b464ae001faf8c3
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-12 17:16:05 +0530

    Added validation criteria in Product, specified error message next to each item in the edit form, defined the style used by errors in the layout page

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault.Models;
 
 namespace SportsStore.Models
 {
     public class Product
     {
         public int ProductID { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
+        
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a valid price")]
         public decimal Price { get; set; }
+        
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category { get; set; }
     }
 }

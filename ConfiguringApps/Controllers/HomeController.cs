aedd58acc2c2cd670242d96770cdb9cbeb66f864
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-13 13:53:40 +0530

    Added HomeController with initial Index action

---
+++ ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() =>
+            View(new Dictionary<string, string> {
+                ["Message"] = "This is the Index action"
+            });
+
+
+    }
+}

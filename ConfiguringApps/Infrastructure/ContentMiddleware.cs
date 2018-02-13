762242ab72e018c683b148a3cd0b2117bc654c04
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-13 16:56:04 +0530

    Injected UptimeService in ContentMiddleware

--- ConfiguringApps/Infrastructure/ContentMiddleware.cs
+++ ConfiguringApps/Infrastructure/ContentMiddleware.cs
@@ -7,14 +7,21 @@ namespace ConfiguringApps.Infrastructure
     public class ContentMiddleware
     {
         private RequestDelegate nextDelegate;
+        private UptimeService uptime;
 
-        public ContentMiddleware(RequestDelegate next) => nextDelegate = next;
+        public ContentMiddleware(RequestDelegate next, UptimeService up)
+        {
+            nextDelegate = next;
+            uptime = up;
+        }
 
         public async Task Invoke(HttpContext httpContext)
         {
             if (httpContext.Request.Path.ToString().ToLower() == "/middleware")
             {
-                await httpContext.Response.WriteAsync("This is from the content middleware", Encoding.UTF8);
+                await httpContext.Response.WriteAsync(
+                    "This is from the content middleware "
+                    + $"(uptime:{uptime.Uptime}ms)", Encoding.UTF8);
             }
             else
             {

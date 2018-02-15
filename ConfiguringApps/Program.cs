52bd454288f7e9c7e82dfa758ce33ed6947c3c18
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-15 13:20:23 +0530

    Created 'development' specific StartupDevelopment class and updated Program to pick environment specific Startup file

--- ConfiguringApps/Program.cs
+++ ConfiguringApps/Program.cs
@@ -43,7 +43,7 @@ namespace ConfiguringApps
                 .UseDefaultServiceProvider((context, options) => {
                     options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
                 })
-                .UseStartup<Startup>()
+                .UseStartup(nameof(ConfiguringApps))
                 .Build();
         }
             
---
+++ ConfiguringApps/StartupDevelopment.cs
@@ -0,0 +1,27 @@
+﻿using ConfiguringApps.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConfiguringApps
+{
+    public class StartupDevelopment
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<UptimeService>();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseBrowserLink();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}

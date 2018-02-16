bf6e21a95df2c9fe560cf1907d3f2cc30bc39dc2
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-16 18:42:25 +0530

    Defined a catchall (can be any name) and it will receive all segments after id; Customer controller updated to test this behaviour

--- UrlsAndRoutes/Controllers/CustomerController.cs
+++ UrlsAndRoutes/Controllers/CustomerController.cs
@@ -11,10 +11,17 @@ namespace UrlsAndRoutes.Controllers
             Action = nameof(Index)
         });
 
-        public ViewResult List() => View("Result", new Result
-        {
-            Controller = nameof(CustomerController),
-            Action = nameof(List)
-        });
+        public ViewResult List(string id) {
+
+            Result r = new Result
+            {
+                Controller = nameof(CustomerController),
+                Action = nameof(List)
+            };
+
+            r.Data["Id"] = id ?? "<no value>";
+            r.Data["catchall"] = RouteData.Values["catchall"];
+            return View("Result", r);
+        }
     }
 }

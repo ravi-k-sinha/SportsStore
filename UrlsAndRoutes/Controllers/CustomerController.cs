43d16d3b6813c6ae8c63a031204960c57618b723
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-20 14:31:35 +0530

    Define 'myroute' for Customer.Index; as a result of this this route will only be reached by /myroute and will not use default route (/Customer/Index)

--- UrlsAndRoutes/Controllers/CustomerController.cs
+++ UrlsAndRoutes/Controllers/CustomerController.cs
@@ -5,6 +5,7 @@ namespace UrlsAndRoutes.Controllers
 {
     public class CustomerController : Controller
     {
+        [Route("myroute")]
         public ViewResult Index() => View("Result", new Result
         {
             Controller = nameof(CustomerController),

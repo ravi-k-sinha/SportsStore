6bc775c68cfe5ffd2650a083f32c46e92817b27d
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-20 14:34:06 +0530

    Used '[controller]' token in the route attribute to used controller name without hard-coding it

--- UrlsAndRoutes/Controllers/CustomerController.cs
+++ UrlsAndRoutes/Controllers/CustomerController.cs
@@ -5,7 +5,7 @@ namespace UrlsAndRoutes.Controllers
 {
     public class CustomerController : Controller
     {
-        [Route("myroute")]
+        [Route("[controller]/MyAction")]
         public ViewResult Index() => View("Result", new Result
         {
             Controller = nameof(CustomerController),

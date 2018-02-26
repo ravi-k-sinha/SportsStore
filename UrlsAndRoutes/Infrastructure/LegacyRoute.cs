4341609d897220cb68345e937d99e95640877c56
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-26 18:33:04 +0530

    Defined outgoing URL generation. In the generator code, it is not required to specify controller and action as when the route matches, appropriate controller and action is specified internally. The default route shows the links appropriately

--- UrlsAndRoutes/Infrastructure/LegacyRoute.cs
+++ UrlsAndRoutes/Infrastructure/LegacyRoute.cs
@@ -36,6 +36,15 @@ namespace UrlsAndRoutes.Infrastructure
 
         public VirtualPathData GetVirtualPath(VirtualPathContext context)
         {
+            if (context.Values.ContainsKey("legacyUrl"))
+            {
+                string url = context.Values["legacyUrl"] as string;
+                if (urls.Contains(url))
+                {
+                    return new VirtualPathData(this, url);
+                }
+            }
+
             return null;
         }
     }

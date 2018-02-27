c8ddb1b324e40a9b850bf9e690b267b1022ac832
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-27 17:51:14 +0530

    Accessing form parameters through Request.Form context object.

---
+++ ControllersAndActions/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("SimpleForm");
+
+        public ViewResult ReceiveForm()
+        {
+            var name = Request.Form["name"];
+            var city = Request.Form["city"];
+
+            return View("Result", $"{name} lives in {city}");
+        }
+    }
+}

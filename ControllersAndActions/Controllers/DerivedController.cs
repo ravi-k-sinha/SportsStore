3a0a7998b1b0568f8af6b7dd053ebf516eb5f0f8
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-27 17:32:10 +0530

    Reading the headers in the request received by the controller

--- ControllersAndActions/Controllers/DerivedController.cs
+++ ControllersAndActions/Controllers/DerivedController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace ControllersAndActions.Controllers
 {
@@ -6,5 +7,8 @@ namespace ControllersAndActions.Controllers
     {
         public ViewResult Index() =>
             View("Result", $"This is a derived controller");
+
+        public ViewResult Headers() => View("DictionaryResult", 
+            Request.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.First()));
     }
 }

6a3ca307603958204f2eb6467082743c6bbe6ce9
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-27 17:13:51 +0530

    Updated POCO controller to use MVC components explicitly to be able to return a ViewResult that is used by MVC to use a Razor view

--- ControllersAndActions/Controllers/PocoController.cs
+++ ControllersAndActions/Controllers/PocoController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +10,14 @@ namespace ControllersAndActions.Controllers
 {
     public class PocoController
     {
-        public string Index() => "This is a POCO controller";
+        public ViewResult Index() => new ViewResult()
+        {
+            ViewName = "Result",
+            ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary())
+            {
+                Model = $"This is a POCO controller"
+            }
+        };
+        
     }
 }

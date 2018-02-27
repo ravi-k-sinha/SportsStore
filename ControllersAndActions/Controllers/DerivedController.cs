d67bc237857da76a455e9872837ed5b4fb3d72da
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-27 17:17:46 +0530

    Inheriting from base Controller gives scaffolding and allows for elegant clutter-free MVC code. DerivedController does the same thing as POCO Controller but without clutter.

---
+++ ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        public ViewResult Index() =>
+            View("Result", $"This is a derived controller");
+    }
+}

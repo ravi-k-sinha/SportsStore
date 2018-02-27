a0bb69da8936ad7aa2927da46056781022441885
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-02-27 16:34:51 +0530

    Added a simple POCO (Plain Old CLR Object) controller

---
+++ ControllersAndActions/Controllers/PocoController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllersAndActions.Controllers
+{
+    public class PocoController
+    {
+        public string Index() => "This is a POCO controller";
+    }
+}

886bcdf3e36ff952e7fda70b0034928a34b11501
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-03-02 13:57:14 +0530

    Added bower config for bootstrap, created a HomeController with default view

---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View();
+    }
+}
